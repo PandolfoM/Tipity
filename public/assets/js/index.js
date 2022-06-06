228a961e96eca386d0149d2a86597d501e61fe09
Matthew Pandolfo <matt@pandolfo.com>
2022-06-06 19:57:49 -0400

    round up

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,17 +2,32 @@ $(document).ready(function () {
   $(".servicePercent").html("5");
 
   $(".billTotalInput").on("input", function () {
-    getValues();
+    getValues($("#roundUpSwitch").is(':checked'));
+  });
+
+  $(".serviceRadio").change(function () { 
+    getValues($("#roundUpSwitch").is(':checked'));
+  });
+
+  $(".roundUpSwitch").change(function () { 
+    getValues($("#roundUpSwitch").is(':checked'));
   });
 });
 
-function getValues() {
-  let billInput = $(".billTotalInput").val();
+function getValues(round) {
   let serviceVal = $("input[type='radio']:checked").val();
-  let getTipTotal = serviceVal * billInput;
-  let getTotal = `1.${serviceVal}` * billInput;
-  $(".totalTip").html((getTipTotal / 100).toFixed(2));
-  $(".totalAmtSpan").html(getTotal.toFixed(2));
+  let billInput = $(".billTotalInput").val();
+  if (round) {
+    let getTipTotal = serviceVal * billInput;
+    let getTotal = `1.${serviceVal}` * billInput;
+    $(".totalTip").html(roundUp(getTipTotal / 100).toFixed(2));
+    $(".totalAmtSpan").html(roundUp(getTotal.toFixed(2)));
+  } else {
+    let getTipTotal = serviceVal * billInput;
+    let getTotal = `1.${serviceVal}` * billInput;
+    $(".totalTip").html((getTipTotal / 100).toFixed(2));
+    $(".totalAmtSpan").html(getTotal.toFixed(2));
+  }
   if (serviceVal == "05") {
     $(".servicePercent").html("5");
   } else {
@@ -28,4 +43,8 @@ function clearInputs() {
   $("input[type=radio]").prop("checked", function () {
     return this.getAttribute("checked") == "checked";
   });
+}
+
+function roundUp(num) {
+  return Math.ceil(num)
 }
