908c7b1fa931ce0ad7ff3b93cc360d6ec01f9c95
Matthew Pandolfo <matt@pandolfo.com>
2022-06-06 19:00:29 -0400

    disaplay tip total and total with tip

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,11 +1,21 @@
-$(document).ready(function() {
-  $(".billTotalInput").on("input",function(){
-    let billInput = $('.billTotalInput').val()
-    let serviceVal = $("input[type='radio']:checked").val()
-    let getTotal = serviceVal*billInput
-    $(".totalAmtSpan").html(insertDecimal(getTotal))
-  })
-})
+$(document).ready(function () {
+  let serviceVal = $("input[type='radio']:checked").val();
+  $(".servicePercent").html(serviceVal);
+
+  $(".billTotalInput").on("input", function () {
+    getValues()
+  });
+});
+
+function getValues() {
+  let billInput = $(".billTotalInput").val();
+  let serviceVal = $("input[type='radio']:checked").val();
+  let getTipTotal = serviceVal * billInput;
+  let getTotal = `1.${serviceVal}` * billInput;
+  $(".totalTip").html(insertDecimal(getTipTotal));
+  $(".totalAmtSpan").html(getTotal.toFixed(2));
+  $(".servicePercent").html(serviceVal);
+}
 
 function insertDecimal(num) {
   return (num / 100).toFixed(2);
