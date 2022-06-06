20a0189b7041b120d55373760f4987470390eb89
Matthew Pandolfo <matt@pandolfo.com>
2022-06-06 18:46:12 -0400

    display total with tip on screen

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
   $(".billTotalInput").on("input",function(){
     let billInput = $('.billTotalInput').val()
     let serviceVal = $("input[type='radio']:checked").val()
-    console.log(serviceVal);
-    $(".totalAmtSpan").text(billInput)
+    let getTotal = serviceVal*billInput
+    $(".totalAmtSpan").html(insertDecimal(getTotal))
   })
 })
+
+function insertDecimal(num) {
+  return (num / 100).toFixed(2);
+}
